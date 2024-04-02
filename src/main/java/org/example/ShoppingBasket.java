2c75ab9cc059537f05312e12a45f2b40bfbc06f9
surachita <surachita.m@gmail.com>
2024-04-02 11:23:41 +0530

    first commit

---
+++ src/main/java/org/example/ShoppingBasket.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.*;
+
+public class ShoppingBasket {
+
+    private static final Map<String, Pair<Double, Offer>> itemsWithPrices = new HashMap<>();
+
+    static {
+        itemsWithPrices.put("Apple", Pair.of(0.35, Offer.NONE));
+        itemsWithPrices.put("Banana", Pair.of(0.20, Offer.NONE));
+        itemsWithPrices.put("Melons", Pair.of(0.50, Offer.BUY_ONE_GET_ONE));
+        itemsWithPrices.put("Limes", Pair.of(0.15, Offer.THREE_FOR_THE_PRICE_OF_TWO));
+    }
+
+
+    static double calculateTotalPrice(Map<String, Integer> basket) {
+        double total = 0.0;
+
+        for (Map.Entry<String, Integer> entry : basket.entrySet()) {
+            String itemInBasket = entry.getKey();
+            if (itemsWithPrices.containsKey(itemInBasket)) {
+                int quantityInBasket = entry.getValue();
+                for (Map.Entry<String, Pair<Double, Offer>> itemEntry : itemsWithPrices.entrySet()) {
+                    Pair<Double, Offer> itemValue = itemEntry.getValue();
+                    if (itemEntry.getKey().equals((itemInBasket))) {
+                        Double price = itemValue.getLeft();
+                        total += calculatePriceWithOffer(quantityInBasket, price, itemValue.getRight());
+                    }
+                }
+
+            } else {
+                System.out.println(itemInBasket + " not present in System");
+            }
+        }
+        return total;
+    }
+
+
+    private static double calculatePriceWithOffer(int quantity, double price, Offer offer) {
+        double totalPrice = 0.0;
+        int remainder = 0;
+        if (offer.equals(Offer.BUY_ONE_GET_ONE)) {
+            int setsOfTwo = quantity / 2;
+            remainder = quantity % 2;
+            // Two items for the price of one
+            totalPrice += setsOfTwo * (1 * price);
+        } else if (offer.equals(Offer.THREE_FOR_THE_PRICE_OF_TWO)) {
+            int setsOfThree = quantity / 3;
+            remainder = quantity % 3;
+            // three items for the price of two
+            totalPrice += setsOfThree * (2 * price);
+        } else {
+            remainder = quantity;
+        }
+        // Remaining items without offer
+        totalPrice += remainder * price;
+        return totalPrice / 100;
+    }
+
+    enum Offer {
+        BUY_ONE_GET_ONE,
+        THREE_FOR_THE_PRICE_OF_TWO,
+        NONE
+    }
+}
---
+++ src/test/java/org/example/ShoppingBasketTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShoppingBasketTest {
+    double delta = 0.0001;
+
+    @Test
+    public void testCalculateTotalPrice_positive() {
+        Map<String, Integer> basket = new HashMap<>();
+        basket.put("Apple", 3);
+        basket.put("Banana", 5);
+        basket.put("Melons", 12);
+        basket.put("Limes", 6);
+        double expectedTotalPrice = (3 * 0.35 + 5 * 0.20 + 6 * 0.50 + 4 * 0.15) / 100;
+        double actualTotalPrice = ShoppingBasket.calculateTotalPrice(basket);
+        assertEquals(expectedTotalPrice, actualTotalPrice, delta);
+    }
+
+    @Test
+    public void testCalculateTotalPrice_withOneItem() {
+        Map<String, Integer> basket = new HashMap<>();
+        basket.put("Limes", 9);
+        double expectedTotalPrice = (6 * 0.15) / 100;
+        double actualTotalPrice = ShoppingBasket.calculateTotalPrice(basket);
+        assertEquals(expectedTotalPrice, actualTotalPrice, delta);
+    }
+
+    @Test
+    public void testCalculateTotalPrice_withTwoItem() {
+        Map<String, Integer> basket = new HashMap<>();
+        basket.put("Limes", 13);
+        basket.put("Melons", 9);
+        double expectedTotalPrice = ((2 * 4 * 0.15) + 0.15 + (4 * 0.50) + 0.50) / 100;
+        double actualTotalPrice = ShoppingBasket.calculateTotalPrice(basket);
+        assertEquals(expectedTotalPrice, actualTotalPrice, delta);
+    }
+
+    @Test
+    public void testCalculateTotalPrice_Negative() {
+        Map<String, Integer> basket = new HashMap<>();
+        basket.put("Orange", 13);
+        basket.put("Melons", 9);
+        double expectedTotalPrice = ((4 * 0.50) + 0.50) / 100;
+        double actualTotalPrice = ShoppingBasket.calculateTotalPrice(basket);
+        assertEquals(expectedTotalPrice, actualTotalPrice, delta);
+    }
+
+}
